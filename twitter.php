686de4245965c68f61b02d220bf6c89518189ac3
Neradoc <neraOnGit@ri1.fr>
2016-10-20 17:12:45 +0200

    script pour utiliser IFTTT pour publier l'image en cours sur twitter

---
+++ twitter.php
@@ -0,0 +1,21 @@
+<?php
+include("twitter-config.php");
+function twitterImage($urlImage) {
+	global $iftMakerKey,$iftRebusChannel;
+	$iftUrl = "https://maker.ifttt.com/trigger/$iftRebusChannel/with/key/$iftMakerKey";
+
+	$data = ["value1" => $urlImage, "value2" => ""];
+	$data_string = json_encode($data);                                                                                   
+
+	$ch = curl_init($iftUrl);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Content-Length: ' . strlen($data_string))
+	);
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+}
