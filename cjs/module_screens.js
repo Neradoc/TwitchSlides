7a7c0f8d879a82ae7c27ee4402e7f2d479fc8bab
Neradoc <neraOnGit@ri1.fr>
2016-11-01 06:46:18 +0100

    changer l'échelle des images avec alt-clic et glisser l'image n'est plus contrainte aux bords de l'écran maintenir shift en déplaçant pour contraindre l'image aux bords de l'écran

--- cjs/module_screens.js
+++ cjs/module_screens.js
@@ -6,6 +6,9 @@ var fh = 1080;
 var scale = 5;
 var movingImage = false;
 var movingStart = [0,0];
+var scalingImage = false;
+var scalingStart = [0,0];
+var scalingDirection = "";
 function base_size(img) {
 	var iw = img.data("width");
 	var ih = img.data("height");
@@ -45,6 +48,7 @@ function base_size(img) {
 		height: h+"px",
 	});
 }
+//
 $(function() {
 	$(".assign").change(function() {
 		var source = $(this).closest(".source");
@@ -170,7 +174,8 @@ $(function() {
 		return false;
 	});
 	//
-	function moveImage(img,deltaX,deltaY) {
+	function moveImage(img,deltaX,deltaY,shift) {
+		if(typeof(shift) == "undefined") shift = false;
 		var parent = img.parent();
 		var imgPos = {
 			top: img.offset().top  - parent.offset().top,
@@ -178,14 +183,16 @@ $(function() {
 		};
 		var newPosX = Math.round(imgPos.left*scale+deltaX*scale);
 		var newPosY = Math.round(imgPos.top *scale+deltaY*scale);
-		// empêcher de sortir à gauche
-		newPosX = Math.max(0, newPosX);
-		newPosY = Math.max(0, newPosY);
-		// empêcher de sortir à droite
-		var fmw = Math.floor(fw-img.width());
-		var fmh = Math.floor(fh-img.height());
-		newPosX = Math.min(fmw, newPosX);
-		newPosY = Math.min(fmh, newPosY);
+		if(shift) {
+			// empêcher de sortir à gauche
+			newPosX = Math.max(0, newPosX);
+			newPosY = Math.max(0, newPosY);
+			// empêcher de sortir à droite
+			var fmw = Math.floor(fw-img.width());
+			var fmh = Math.floor(fh-img.height());
+			newPosX = Math.min(fmw, newPosX);
+			newPosY = Math.min(fmh, newPosY);
+		}
 		img.css({
 			left: newPosX+"px",
 			top:  newPosY+"px",
@@ -220,7 +227,7 @@ $(function() {
 	//
 	$(".pos_btn.zoomin").click(function() {
 		var img = $(this).siblings(".image");
-		zoom = $(this).siblings(".zoom");
+		var zoom = $(this).siblings(".zoom");
 		if(zoom.val() == 0) zoom.val(img.data("width")/img.width());
 		zoom.val(zoom.val()*1.1);
 		img.height(img.data("height")*zoom.val());
@@ -230,7 +237,7 @@ $(function() {
 	});
 	$(".pos_btn.zoomout").click(function() {
 		var img = $(this).siblings(".image");
-		zoom = $(this).siblings(".zoom");
+		var zoom = $(this).siblings(".zoom");
 		if(zoom.val() == 0) zoom.val(img.width()/img.data("width"));
 		zoom.val(zoom.val()*0.9);
 		img.height(img.data("height")*zoom.val());
@@ -240,7 +247,7 @@ $(function() {
 	});
 	$(".pos_btn.zoomzero").click(function() {
 		var img = $(this).siblings(".image");
-		zoom = $(this).siblings(".zoom");
+		var zoom = $(this).siblings(".zoom");
 		img.data("zoom",0);
 		base_size(img);
 		img.height(img.data("height")*zoom.val());
@@ -248,14 +255,81 @@ $(function() {
 		$(this).closest(".screen").addClass("modified");
 		return false;
 	});
+	//
+	function scaleImage(img,deltaX,deltaY) {
+		var parent = img.parent();
+		var zoom = img.siblings(".zoom");
+		// orienter les deltas selon la direction
+		switch(scalingDirection) {
+		case "topleft":
+			deltaX = -deltaX;
+			deltaY = -deltaY;
+			break;
+		case "topright":
+			deltaY = -deltaY;
+			break;
+		case "bottomleft":
+			deltaX = -deltaX;
+			break;
+		}
+		// récupérer la largeur et la hauteur
+		// changer de delta max en conservant les proportions
+		var newWidth = img.width()+deltaX*scale;
+		var newHeight = img.height()+deltaY*scale;
+		if(newWidth/newHeight > img.width()/img.height()) {
+			newHeight = newWidth*img.height()/img.width();
+			deltaY = (newHeight - img.height()) / scale;
+		} else {
+			newWidth = newHeight*img.width()/img.height();
+			deltaX = (newWidth - img.width()) / scale;
+		}
+		zoom.val(img.width()/img.data("width"));
+		// calculer la position pour passer en mode top/left
+		var imgPos = {
+			top: img.offset().top  - parent.offset().top,
+			left:img.offset().left - parent.offset().left,
+		};
+		var posX = imgPos.left*scale;
+		var posY = imgPos.top *scale;
+		// calculer les nouvelles coordonnées selon la direction
+		switch(scalingDirection) {
+		case "topleft":
+			posX -= deltaX*scale;
+			posY -= deltaY*scale;
+			break;
+		case "topright":
+			posY -= deltaY*scale;
+			break;
+		case "bottomleft":
+			posX -= deltaX*scale;
+			break;
+		}
+		//
+		img.css({
+			width: Math.round(newWidth)+"px",
+			height:Math.round(newHeight)+"px",
+			left:  Math.round(posX)+"px",
+			top:   Math.round(posY)+"px",
+			bottom:"auto",
+			right:"auto",
+		});
+	}
 	// déplacement manuel des images
 	function movingImageMouseMove(evt) {
 		if(movingImage != false) {
 			var curPos = [evt.pageX,evt.pageY];
 			var deltaX = curPos[0]-movingStart[0];
 			var deltaY = curPos[1]-movingStart[1];
-			moveImage(movingImage,deltaX,deltaY);
+			moveImage(movingImage,deltaX,deltaY,evt.shiftKey);
 			movingStart = curPos;
+			return false;
+		}
+		if(scalingImage != false) {
+			var curPos = [evt.pageX,evt.pageY];
+			var deltaX = curPos[0]-scalingStart[0];
+			var deltaY = curPos[1]-scalingStart[1];
+			scaleImage(scalingImage,deltaX,deltaY);
+			scalingStart = curPos;
 			return false;
 		}
 	}
@@ -264,11 +338,43 @@ $(function() {
 			movingImageMouseMove(evt);
 			movingImage.closest(".screen").addClass("modified");
 		}
+		if(scalingImage) {
+			movingImageMouseMove(evt);
+			scalingImage.closest(".screen").addClass("modified");
+		}
+		scalingImage = false;
 		movingImage = false;
 	}
 	$(".pimage .image").mousedown(function(evt) {
-		movingImage = $(this);
-		movingStart = [evt.pageX,evt.pageY];
+		if(evt.altKey) {
+			movingImage = false;
+			scalingImage = $(this);
+			scalingStart = [evt.pageX,evt.pageY];
+			//
+			var parent = scalingImage.parent();
+			var imgPos = {
+				top: scalingImage.offset().top  - parent.offset().top,
+				left:scalingImage.offset().left - parent.offset().left,
+			};
+			ptTouch = {
+				x: (evt.pageX - scalingImage.offset().left)
+				/ scalingImage.width() * scale,
+				y: (evt.pageY - scalingImage.offset().top)
+				/ scalingImage.height() * scale,
+			};
+			if(ptTouch.x<0.5 && ptTouch.y<0.5)
+				scalingDirection = "topleft";
+			else if(ptTouch.x>=0.5 && ptTouch.y<0.5)
+				scalingDirection = "topright";
+			else if(ptTouch.x<0.5 && ptTouch.y>=0.5)
+				scalingDirection = "bottomleft";
+			else
+				scalingDirection = "bottomright";
+		} else {
+			scalingImage = false;
+			movingImage = $(this);
+			movingStart = [evt.pageX,evt.pageY];
+		}
 		return false;
 	});
 	$(".pimage .image, .pimage").mousemove(movingImageMouseMove);
