f58aa267d10b528c56639bb7e36566d93efec3fa
Neradoc <neraOnGit@ri1.fr>
2016-11-08 05:48:51 +0100

    masquer la prévisu vide l'iframe, de façon à éviter que les scripts tournent

--- app/module_previsu.php
+++ app/module_previsu.php
@@ -3,6 +3,6 @@ function disp_previsu($thisurl) {
 	global $Nscreens,$prefs,$url_miniature_stream;
 	?><div id="previsu">
 		<h3><a href="slide" target="_blank">Prévisu</a></h3>
-		<iframe class="pimage" src="slide?debug">...</iframe>
+		<iframe class="pimage" data-src="slide?debug" src="slide?debug">...</iframe>
 	</div><?
 }
--- cjs/gestion.js
+++ cjs/gestion.js
@@ -14,21 +14,26 @@ $(function() {
 	if(previsu === "1") {
 		$("#menu .previsu").prop("checked","1");
 		$("#previsu").show();
+		$("#previsu .pimage").attr("src",$("#previsu .pimage").data("src"));
 	} else if (previsu === "0") {
 		$("#menu .previsu").prop("checked","")
 		$("#previsu").hide();
+		$("#previsu .pimage").attr("src","");
 	} else {
 		$("#menu .previsu").prop("checked","1");
 		$("#previsu").show();
+		$("#previsu .pimage").attr("src",$("#previsu .pimage").data("src"));
 	}
 	$("#menu .previsu").on("click",function() {
 		console.log($(this).prop("checked"));
 		if($(this).prop("checked")) {
 			prefs("activer_previsu","1");
 			$("#previsu").show();
+			$("#previsu .pimage").attr("src",$("#previsu .pimage").data("src"));
 		} else {
 			prefs("activer_previsu","0");
 			$("#previsu").hide();
+			$("#previsu .pimage").attr("src","");
 		}
 	});
 });
